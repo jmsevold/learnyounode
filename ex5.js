1bc612edf89120f9b587f0418eacc5f8acdb85c9
Jonathan Sevold <jmsevold@gmail.com>
2015-09-03 15:50:53 -0700

    ex5 done!

---
+++ ex5.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+var path = require('path')
+var filesPath = process.argv[2];
+var extension = "." + process.argv[3];
+
+
+
+fs.readdir(filesPath,function (err,files) {
+	if (err)  console.log(err);
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		var ext = path.extname(file);
+		if(ext === extension){
+			console.log(file);
+		}
+	};
+})
+
